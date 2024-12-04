d0ea3e437960354476d58ee05b8974b5149f3bbb
prabowo-sakti <admin@prabowosakti.com>
2024-12-04 18:23:33 +0700

    Menjelajahi Objek Date di JavaScript

---
+++ tanggalRilisJavascript.js
@@ -0,0 +1,15 @@
+const jsDateAnnouncement = new Date(818031600000)
+const currentDate = new Date()
+
+const diff = jsDateAnnouncement - currentDate
+
+const formatter = new Intl.RelativeTimeFormat('en', {numeric: 'auto'})
+
+const diffInDays = Math.round(diff / 86400000)
+const diffInYears = Math.round(diffInDays /365)
+
+const diffInText = formatter.format(diffInDays, 'day')
+
+console.log(`Javascript was presented to the world ${formatter.format(diffInDays, 'day')}`)
+
+console.log(`Javascript was presented to the world ${formatter.format(diffInYears, 'year')}`)
