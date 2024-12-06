83c1c667e8e9f81ce86c0bef5d0b9be31e998cbb
prabowo-sakti <admin@prabowosakti.com>
2024-12-06 21:55:31 +0700

    add arraySections learning

---
+++ arraySection.js
@@ -0,0 +1,19 @@
+//Note: semakin ke bawah, berarti saya mempelajarinya semakin mendalam
+
+// const data = [1, [2, 3], [4, 5], [6, [7, 8, [7]]]];
+
+// const flatData = data.flat(Infinity);
+
+// console.log(flatData.map((num) => num * 2));
+
+// const people = ["prabowo", "sakti", "ngoding", "jack"];
+
+// console.log(people.join(" + "));
+
+// let arr = [1, 2, 2, 3, 1, 4, 5, 4, 5];
+
+// let set = new Set(arr);
+
+// let uniques = Array.from(set);
+
+// console.log(uniques);
--- tanggalRilisJavascript.js
+++ tanggalRilisJavascript.js
@@ -1,15 +1,16 @@
-const jsDateAnnouncement = new Date(818031600000)
+// const jsDateAnnouncement = new Date(818031600000)
+
 const currentDate = new Date()
+console.log(currentDate)
+// const diff = jsDateAnnouncement - currentDate
 
-const diff = jsDateAnnouncement - currentDate
+// const formatter = new Intl.RelativeTimeFormat('en', {numeric: 'auto'})
 
-const formatter = new Intl.RelativeTimeFormat('en', {numeric: 'auto'})
+// const diffInDays = Math.round(diff / 86400000)
+// const diffInYears = Math.round(diffInDays /365)
 
-const diffInDays = Math.round(diff / 86400000)
-const diffInYears = Math.round(diffInDays /365)
+// const diffInText = formatter.format(diffInDays, 'day')
 
-const diffInText = formatter.format(diffInDays, 'day')
+// console.log(`Javascript was presented to the world ${formatter.format(diffInDays, 'day')}`)
 
-console.log(`Javascript was presented to the world ${formatter.format(diffInDays, 'day')}`)
-
-console.log(`Javascript was presented to the world ${formatter.format(diffInYears, 'year')}`)
+// console.log(`Javascript was presented to the world ${formatter.format(diffInYears, 'year')}`)
